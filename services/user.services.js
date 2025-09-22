de62b0305fe8505075d6e3c6f866dade13801ddd
a.novikov <anovikov@salerow.software>
2025-09-22 21:53:34 +0300

    [refactor] remove object 'next', add parseInt salt rounds, trow error validation

--- services/user.services.js
+++ services/user.services.js
@@ -1,24 +1,24 @@
 import bcrypt from 'bcrypt';
 import { ErrorValidation } from '../middlewares/errorHandler.js';
+import {addAndSaveNewUser} from '../repositories/user.repositories.js';
 import {ROLES} from '../roles.js';
 
 
-const saltRounds = process.env.BCRYPT_SALT_ROUNDS;
+const saltRounds = parseInt(process.env.BCRYPT_SALT_ROUNDS);
 
-export const createNewUser = async ({email, password, role=ROLES.CUSTOMER}, next) => {
+export const createNewUser = async ({email, password, role=ROLES.CUSTOMER}) => {
     try {
-        const hash = await bcrypt(password, saltRounds);
+        const hash = await bcrypt.hash(password, saltRounds);
         const newUser = await addAndSaveNewUser({
             email,
             password: hash,
             role
         });
-
         return {
             id: newUser.id,
             email: newUser.email
         };
     } catch(err) {
-        next(new ErrorValidation(err));
+        throw new ErrorValidation(err);
     };
 };
