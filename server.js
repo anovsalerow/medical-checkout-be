cbb66f91cc9b87b210a65915e2e8994c82241bbc
a.novikov <anovikov@salerow.software>
2025-09-22 18:51:21 +0300

    [feature] create server.js, create mongodb connection

---
+++ server.js
@@ -0,0 +1,20 @@
+import express, {Router} from 'express';
+import 'dotenv/config';
+import mongoose from 'mongoose';
+
+const app = express();
+const PORT = process.env.PORT;
+const apiRouter = Router();
+
+const MONGO_USERNAME = process.env.MONGO_USERNAME
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD
+const MONGO_DB_NAME = process.env.MONGO_DB_NAME;
+const URI_DB = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.k6lea.mongodb.net/${MONGO_DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose
+    .connect(URI_DB)
+    .then(() => console.log('MongoDB connected'))
+    .catch((err) => console.log(`DB connection error: ${err}`));
+    
+
+app.listen(PORT, console.log(`Listening on port ${PORT}`))
