92609b99e4879e30a7cd28212052455acea82235
a.novikov <anovikov@salerow.software>
2025-09-22 21:51:46 +0300

    [refactor] remove object 'next', correct res status code 2001 -> 201

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -1,7 +1,7 @@
 import { createNewUser } from "../services/user.services.js";
 
-export const signUp = async (req, res, next) => {
-    const newUser = await createNewUser(req.body, next);
+export const signUp = async (req, res) => {
+    const newUser = await createNewUser(req.body);
 
-    return res.status(2001).json(newUser);
+    return res.status(201).json(newUser);
 };
