224b72635c7a3fffb094aefaa103aa87993a48ca
a.novikov <anovikov@salerow.software>
2025-09-22 19:15:19 +0300

    [feature] create errorHandler with status code ErrorUnAuthorized(401), ErrorForbidden(403), ErrorObjectNotFound(404), ErrorUserAlreadyExist(409)

---
+++ middlewares/errorHandler.js
@@ -0,0 +1,30 @@
+export class ErrorHandler extends Error {
+    constructor(status, message) {
+        super(message);
+        this.statusCode = status;
+    };
+};
+
+export class ErrorUnAuthorized extends ErrorHandler {
+    constructor(message) {
+        super(401, message);
+    };
+};
+
+export class ErrorForbidden extends ErrorHandler {
+    constructor(message) {
+        super(403, message);
+    };
+};
+
+export class ErrorObjectNotFound extends ErrorHandler {
+    constructor(message) {
+        super(404, message);
+    };
+};
+
+export class ErrorUserAlreadyExist extends ErrorHandler {
+    constructor(message) {
+        super(409, message);
+    };
+};
