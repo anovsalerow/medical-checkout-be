0ece78138d9a1a2f8aea4dc8c80c7ee06b2b4bf6
a.novikov <anovikov@salerow.software>
2025-09-23 17:54:58 +0300

    [feature] create user service 'signInUser',  add 'return next...' into 'createNewUser'

--- services/user.services.js
+++ services/user.services.js
@@ -1,12 +1,16 @@
 import bcrypt from 'bcrypt';
 import { ErrorValidation } from '../middlewares/errorHandler.js';
-import {addAndSaveNewUser} from '../repositories/user.repositories.js';
+import {
+    addAndSaveNewUser, 
+    getUserByEmailAndPassword
+} from '../repositories/user.repositories.js';
+import { createAccessToken, createRefreshToken } from '../utils/token.utils.js';
 import {ROLES} from '../roles.js';
 
 
 const saltRounds = parseInt(process.env.BCRYPT_SALT_ROUNDS);
 
-export const createNewUser = async ({email, password, role=ROLES.CUSTOMER}) => {
+export const createNewUser = async ({email, password, role=ROLES.CUSTOMER}, next) => {
     try {
         const hash = await bcrypt.hash(password, saltRounds);
         const newUser = await addAndSaveNewUser({
@@ -19,6 +23,20 @@ export const createNewUser = async ({email, password, role=ROLES.CUSTOMER}) => {
             email: newUser.email
         };
     } catch(err) {
-        throw new ErrorValidation(err);
+        return next ( new ErrorValidation(err));
+    };
+};
+
+export const signInUser = async ({email, password}, next) => {
+    const userInfo = await getUserByEmailAndPassword({email, password});
+    if (!userInfo) {
+        return next(new ErrorValidation('Incorrect email or password'));
+    };
+    const accessToken = createAccessToken(userInfo);
+    const refreshToken = createRefreshToken(userInfo);
+    return {
+        accessToken,
+        refreshToken,
+        userId: userInfo.id
     };
 };
