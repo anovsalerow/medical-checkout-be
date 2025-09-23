1faf1ab9d5dc631b7366c5d95b9ab2c93b9b9c89
a.novikov <anovikov@salerow.software>
2025-09-23 20:34:56 +0300

    [feature] create cookie config

---
+++ config/cookie.config.js
@@ -0,0 +1,10 @@
+export const cookieConfig = {
+    access: {
+        httpOnly: true,
+        maxAge: 60 * 60 * 1000
+    },
+    refresh: {
+        httpOnly: true,
+        maxAge: 7 * 24 * 60 * 60 * 1000
+    }
+};
