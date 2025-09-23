1a70cf5f75489395b0a8feb003136501d2c71d0f
a.novikov <anovikov@salerow.software>
2025-09-23 11:15:48 +0300

    [feature] create query getUserByEmail

--- repositories/user.repositories.js
+++ repositories/user.repositories.js
@@ -3,3 +3,14 @@ import {User} from '../models/user.js';
 export const addAndSaveNewUser = async (newUser) => {
     return await new User(newUser).save();
 };
+
+export const getUserByEmail = async(email) => {
+    return User.findOne(
+        {email}, 
+        {
+            email: 1, 
+            role: 1, 
+            refreshToken: 1
+        }
+    );
+};
