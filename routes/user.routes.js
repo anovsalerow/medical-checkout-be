e11351a7c723ee4e76a978aab41c1991f65fa0b7
a.novikov <anovikov@salerow.software>
2025-09-23 14:46:58 +0300

    [feature] add user routes 'login

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,11 @@
 import {Router} from 'express';
-import { signUp } from '../controllers/user.controllers.js';
+import { signUp, signIn } from '../controllers/user.controllers.js';
 import { signUpMiddlewareArray } from '../middlewares/user.middleware.js';
 
 
 const router = Router();
 
 router.post('/register', signUpMiddlewareArray, signUp);
+router.post('/login', signIn);
 
 export default router;
