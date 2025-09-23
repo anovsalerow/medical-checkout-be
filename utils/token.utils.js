b534771ec848208ef2fc6e06822eb72239f94b08
a.novikov <anovikov@salerow.software>
2025-09-23 19:16:01 +0300

    [feature] create token utils verifyToken, verifyAccessToken, verifyRefreshToken

--- utils/token.utils.js
+++ utils/token.utils.js
@@ -5,10 +5,21 @@ const generateToken = (payload, secret, expiresIn) => {
     return jwt.sign(payload, secret, {expiresIn});
 };
 
+const verifyToken = (token, secret) => {
+    return jwt.verify(token, secret);
+};
+
 export const createAccessToken = (payload) => {
     return generateToken(payload, tokenConfig.access.secret, tokenConfig.access.expiresIn);
 };
 
 export const createRefreshToken = (payload) => {
     return generateToken(payload, tokenConfig.refresh.secret, tokenConfig.refresh.epiresIn);
+};
+
+export const verifyAccessToken = (accessToken) => {
+    return verifyToken(accessToken, tokenConfig.access.secret);
+};
+export const verifyRefreshToken = (refreshToken) => {
+    return verifyToken(refreshToken, tokenConfig.refresh.secret);
 };
