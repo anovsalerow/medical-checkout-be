ac93fc7220ac915af7d33476e7504b155f95601b
a.novikov <anovikov@salerow.software>
2025-09-23 19:20:47 +0300

    [feature] add logout endpoint

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,5 @@
 import {Router} from 'express';
-import { signUp, signIn } from '../controllers/user.controllers.js';
+import { signUp, signIn, signOut } from '../controllers/user.controllers.js';
 import { signUpMiddlewareArray } from '../middlewares/user.middleware.js';
 
 
@@ -7,5 +7,6 @@ const router = Router();
 
 router.post('/register', signUpMiddlewareArray, signUp);
 router.post('/login', signIn);
+router.post('/logout', signOut);
 
 export default router;
