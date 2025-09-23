c2f406addafcf6f08350d87b8feb4a48ea2102a9
a.novikov <anovikov@salerow.software>
2025-09-23 20:36:55 +0300

    [feature] add renewal tokens endpoint

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,12 +1,16 @@
 import {Router} from 'express';
-import { signUp, signIn, signOut } from '../controllers/user.controllers.js';
+import { signUp, signIn, signOut, renewalToken } from '../controllers/user.controllers.js';
 import { signUpMiddlewareArray } from '../middlewares/user.middleware.js';
+import { isAuthorized } from '../middlewares/auth.middleware.js';
+import { ROLES } from '../roles.js';
 
+const allRoles = Object.values(ROLES);
 
 const router = Router();
 
 router.post('/register', signUpMiddlewareArray, signUp);
 router.post('/login', signIn);
 router.post('/logout', signOut);
+router.post('/renewal', isAuthorized(allRoles), renewalToken);
 
 export default router;
