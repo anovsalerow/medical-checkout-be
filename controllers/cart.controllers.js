740f71eed3090d53be298519e5c63a027ed7706b
a.novikov <anovikov@salerow.software>
2025-09-24 22:47:00 +0300

    [feature] create cart controllers addProductByIdToCart, removeProductByIdFromCart, checkoutOrder

---
+++ controllers/cart.controllers.js
@@ -0,0 +1,29 @@
+import {addProductToCart, createCheckoutOrder, removeProductFromCart} from "../services/cart.services.js";
+import {getUserId} from "../services/user.services.js";
+
+export const addProductByIdToCart = async (req, res, next) => {
+    try {
+        const cart = await addProductToCart(getUserId(req), req.params);
+        res.status(200).json(cart);
+    } catch (err) {
+        next(err);
+    }
+};
+
+export const removeProductByIdFromCart = async (req, res, next) => {
+    try {
+        const cart = await removeProductFromCart(getUserId(req), req.params);
+        res.status(200).json(cart);
+    } catch (err) {
+        next(err);
+    }
+};
+
+export const checkoutOrder = async (req, res, next) => {
+    try {
+        const order = await createCheckoutOrder(getUserId(req));
+        res.status(200).json(order);
+    } catch (err) {
+        next(err);
+    }
+};
