e4f9b6d0f9f9cd25cc4d87ba1e694e4cd1a76c69
a.novikov <anovikov@salerow.software>
2025-09-24 22:44:32 +0300

    [refactor] auth middlewware: import error necessary handlers ErrorUnAuthorized and ErrorForbidden, fix name mistakes

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,14 +1,15 @@
 import { verifyAccessToken } from "../utils/token.utils.js";
+import {ErrorUnAuthorized, ErrorForbidden} from '../middlewares/errorHandler.js';
 
 export const isAuthorized = (roles) => (req, res, next) => {
     const accessToken = req?.cookies?.accessToken;
     if (!accessToken) {
-        return next(new ErrorUnauthorized('No token provided'));
+        return next(new ErrorUnAuthorized('No access token provided'));
     }
-    
+
     const decoded = verifyAccessToken(accessToken);
     if (!decoded) {
-        return next(new ErrorUnauthorized('Invalid token'));
+        return next(new ErrorUnAuthorized('Invalid token'));
     }
     if (!roles.includes(decoded?.role)) {
         return next(new ErrorForbidden('Access denied'));
