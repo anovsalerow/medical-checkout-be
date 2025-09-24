8028645a44c7a889497315b28802114e2bd42232
a.novikov <anovikov@salerow.software>
2025-09-24 22:48:50 +0300

    [feature] create cart repositories: getProductsFromCartByUserId, findCartByUserIdAndUpdate, findOrderByUserIdAndUpdate, findCartByUserId

---
+++ repositories/cart.repositories.js
@@ -0,0 +1,37 @@
+import {Cart} from "../models/cart.js";
+import {Order} from "../models/order.js";
+
+export const getProductsFromCartByUserId = async (userId) => {
+    return Cart.findOne({userId}).populate('items.productId', '_id title price color formFactor');
+};
+
+export const findCartByUserIdAndUpdate = async (userId, newData, {arrayFilters}) => {
+    const options = {
+        new: true,
+        upsert: true,
+        arrayFilters
+    }
+    return Cart.findOneAndUpdate(
+        {userId},
+        newData,
+        options
+    ).populate('items', '_id title price color formFactor');
+};
+
+export const findOrderByUserIdAndUpdate = async (userId, newData) => {
+    const options = {
+        new: true,
+        upsert: true
+    }
+
+    return Order.findOneAndUpdate(
+        {userId},
+        newData,
+        options
+    )
+        .populate('userId', '_id email');
+};
+
+export const findCartByUserId = async (userId) => {
+    return Cart.findOne({userId});
+};
