ee71d6f225ef769b427dd8d0c4af0db471d21c26
a.novikov <anovikov@salerow.software>
2025-09-24 22:45:10 +0300

    [feature] create order model

---
+++ models/order.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const orderSchema = new Schema({
+    userId: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
+    products: [{
+        productId: {
+            type: Schema.Types.ObjectId,
+            required: true,
+            ref: 'Product'
+        },
+        title: {
+            type: String,
+            required: true
+        },
+        price: {
+            type: Number,
+            required: true
+        },
+        quantity: {
+            type: Number,
+            required: true
+        }
+    }],
+    totalPrice: {
+        type: Number,
+        required: true,
+        min: 0
+    }
+});
+
+export const Order = mongoose.model('Order', orderSchema);
