71a1dcb2e483d069be47fc831c0de6fe307fecbd
a.novikov <anovikov@salerow.software>
2025-09-26 13:01:07 +0300

    [feature] create cart route get  to get cart info

--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -1,6 +1,11 @@
 import {Router} from 'express';
 import {isAuthorized} from "../middlewares/auth.middleware.js";
-import {addProductByIdToCart, checkoutOrder, removeProductByIdFromCart} from "../controllers/cart.controllers.js";
+import {
+    addProductByIdToCart, 
+    checkoutOrder, 
+    removeProductByIdFromCart,
+    fetchCart
+} from "../controllers/cart.controllers.js";
 import {ROLES} from "../roles.js";
 
 const router = Router();
@@ -9,5 +14,6 @@ const allRoles = Object.values(ROLES);
 router.put('/:productId', isAuthorized(allRoles), addProductByIdToCart);
 router.delete('/:productId', isAuthorized(allRoles), removeProductByIdFromCart);
 router.post('/checkout', isAuthorized(allRoles), checkoutOrder);
+router.get('/', isAuthorized(allRoles), fetchCart);
 
 export default router;
