21892e2d2bcf4d8a75c44ed83f48c95db254e8e5
a.novikov <anovikov@salerow.software>
2025-09-27 16:36:36 +0300

    [hotfix]rename params SameSite -> samesite

--- config/cookie.config.js
+++ config/cookie.config.js
@@ -2,11 +2,13 @@ export const cookieConfig = {
     access: {
         httpOnly: true,
         maxAge: 60 * 60 * 1000,
-        SameSite: None
+        sameSite: "None",
+        secure: true
     },
     refresh: {
         httpOnly: true,
         maxAge: 7 * 24 * 60 * 60 * 1000,
-        SameSite: None
+        sameSite: "None",
+        secure: true
     }
 };
