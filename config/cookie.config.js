a86914e93b8e6554edcba04911640ff01ae29555
a.novikov <anovikov@salerow.software>
2025-09-27 16:30:32 +0300

    [refactor] cookie config: same site = none

--- config/cookie.config.js
+++ config/cookie.config.js
@@ -1,10 +1,12 @@
 export const cookieConfig = {
     access: {
         httpOnly: true,
-        maxAge: 60 * 60 * 1000
+        maxAge: 60 * 60 * 1000,
+        SameSite: none
     },
     refresh: {
         httpOnly: true,
-        maxAge: 7 * 24 * 60 * 60 * 1000
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        SameSite: None
     }
 };
