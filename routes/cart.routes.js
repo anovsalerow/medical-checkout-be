427322f20415585f9518c943c137cc44ea224086
a.novikov <anovikov@salerow.software>
2025-09-27 13:34:53 +0300

    [feature] create cart routes saveOrder, addDemoProducts

--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -4,13 +4,17 @@ import {
     addProductByIdToCart, 
     checkoutOrder, 
     removeProductByIdFromCart,
-    fetchCart
+    fetchCart,
+    saveOrder,
+    addDemoProducts
 } from "../controllers/cart.controllers.js";
 import {ROLES} from "../roles.js";
 
 const router = Router();
 const allRoles = Object.values(ROLES);
 
+router.put('/orders', isAuthorized(allRoles), saveOrder);
+router.put('/demo', isAuthorized(allRoles), addDemoProducts);
 router.put('/:productId', isAuthorized(allRoles), addProductByIdToCart);
 router.delete('/:productId', isAuthorized(allRoles), removeProductByIdFromCart);
 router.post('/checkout', isAuthorized(allRoles), checkoutOrder);
